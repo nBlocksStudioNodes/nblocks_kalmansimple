a000c5d51872bedab6b4bd8d39ec60deee347ffd
nikos chalikias <nikos.chalikias@gmail.com>
2021-03-31 14:16:59 +0100

    initial testing pass

--- kalmanSimple.cpp
+++ kalmanSimple.cpp
@@ -3,12 +3,13 @@
 
 nBlock_KalmanSimple::nBlock_KalmanSimple(float e_mea, float e_est, float q):
 	_kalman(e_mea, e_est, q) {
+    outputType[0] = OUTPUT_TYPE_FLOAT;
 }
 
 void nBlock_KalmanSimple::triggerInput(nBlocks_Message message){
     if (message.inputNumber == 0) {         // new feedback-value at Input1 (Schematic Input 2)
         if (message.dataType == OUTPUT_TYPE_FLOAT) {
-            _input = message.floatValue;      
+            _input = message.floatValue;
             newValueFLAG = 1;
         }
     }
@@ -18,7 +19,7 @@ void nBlock_KalmanSimple::endFrame(void) {
 	if(newValueFLAG){
 		newValueFLAG = 0;
 		_estimated_value = _kalman.updateEstimate(_input);
-		output[0] = _estimated_value;
+		output[0] = PackFloat(_estimated_value);
 		available[0] = 1;
 	}	
 }
